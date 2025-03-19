7d8370921dcc031436bc8dfdf61cd646c97b92c3
devsyw <devsyw@gmail.com>
2025-03-19 13:44:31 +0900

    [init] 스켈레톤 셋팅

---
+++ src/main/java/com/lezhin/lezhinchallenge/LezhinChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.lezhin.lezhinchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LezhinChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LezhinChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lezhin/lezhinchallenge/LezhinChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lezhin.lezhinchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LezhinChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
