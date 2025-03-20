2045a496c2a2218ebce75bd2a6b25bce14d3b686
서영우 <seo_youngwoo02@eland.co.kr>
2025-03-20 16:14:21 +0900

    [fix] SecurityConfig - @EnableMethodSecurity 수정

--- src/main/java/com/lezhin/lezhinchallenge/common/config/SecurityConfig.java
+++ src/main/java/com/lezhin/lezhinchallenge/common/config/SecurityConfig.java
@@ -24,7 +24,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  */
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
